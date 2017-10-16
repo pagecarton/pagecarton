519e75a439bf4e295af4f8ce20f0b197c7fa48e9
Ayoola FALOLA <ayoola@ayoo.la>
2017-10-16 20:26:44 +0100

    Version to 1.7.8

--- pagecarton/core/library/PageCarton.php
+++ pagecarton/core/library/PageCarton.php
@@ -25,7 +25,7 @@ class PageCarton extends Ayoola_Application
      *
      * @var string
      */
-	const VERSION = '1.7.7';
+	const VERSION = '1.7.8';
 
 	// END OF CLASS
 }
---
+++ pagecarton/core/library/PageCarton/Form/Element.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * PageCarton Content Management System
+ *
+ * LICENSE
+ *
+ * @category   PageCarton CMS
+ * @package    PageCarton_Form_Element
+ * @copyright  Copyright (c) 2017 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: Element.php Monday 16th of October 2017 02:31PM ayoola@ayoo.la $
+ */
+
+/**
+ * @see PageCarton_Widget
+ */
+
+class PageCarton_Form_Element extends Ayoola_Form_Element
+{
+	
+	// END OF CLASS
+}
