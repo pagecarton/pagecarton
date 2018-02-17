f57c9b048a8a707d09824cd446c4d46b22dd2cc6
Ayoola FALOLA <ayoola@ayoo.la>
2018-02-18 00:16:54 +0100

    .

---
+++ pagecarton/core/library/PageCarton/Email/OptIn - Copy.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PageCarton Content Management System
+ *
+ * LICENSE
+ *
+ * @category   PageCarton CMS
+ * @package    PageCarton_Email_OptIn
+ * @copyright  Copyright (c) 2017 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: OptIn.php Monday 16th of October 2017 01:02PM ayoola@ayoo.la $
+ */
+
+/**
+ * @see PageCarton_Widget
+ */
+
+class PageCarton_Email_OptIn extends Application_User_UserEmail_Creator
+{
+	
+
+	// END OF CLASS         
+}
