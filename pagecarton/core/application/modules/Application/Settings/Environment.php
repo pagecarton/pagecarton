9ee633d11dadccfb8461b928f61cfa50a0b57ac1
Ayoola FALOLA <ayoola@ayoo.la>
2018-05-14 19:32:15 +0100

    Environment mode added

---
+++ pagecarton/core/application/modules/Application/Settings/Environment.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * PageCarton Content Management System
+ *
+ * LICENSE
+ *
+ * @category   PageCarton CMS
+ * @package    Application_Settings_Environment
+ * @copyright  Copyright (c) 2011-2016 PageCarton (http://www.pagecarton.com)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: System.php 5.7.2012 11.53 ayoola $
+ */
+
+/**
+ * @see Ayoola_Abstract_Playable
+ */
+ 
+require_once 'Ayoola/Abstract/Playable.php';
+
+
+/**
+ * @category   PageCarton CMS
+ * @package    Application_Settings_Environment
+ * @copyright  Copyright (c) 2011-2016 PageCarton (http://www.pagecarton.com)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+class Application_Settings_Environment extends Application_Settings_Abstract
+{
+	
+    /**
+     * creates the form for creating and editing
+     * 
+     * param string The Value of the Submit Button
+     * param string Value of the Legend
+     * param array Default Values
+     */
+	public function createForm( $submitValue, $legend = null, Array $values = null )
+    {
+		$settings = unserialize( @$values['settings'] );
+        $form = new Ayoola_Form( array( 'name' => $this->getObjectName() ) );
+		$form->submitValue = $submitValue ;
+		$form->oneFieldSetAtATime = true;
+		$fieldset = new Ayoola_Form_Element;
+	//	$fieldset->placeholderInPlaceOfLabel = true;
+		
+		$fieldset->addRequirements( array( 'WordCount' => array( 12, 100 ) ) );
+		$options = array( 
+							'development' => 'Development',
+							'production' => 'Production' 
+						);
+		$fieldset->addElement( array( 'name' => 'mode', 'label' => 'Mode', 'value' => @$settings['mode'], 'type' => 'Radio' ), $options );
+	//	$fieldset->addRequirement( 'options', array( 'ArrayKeys' => $options ) );
+		$fieldset->addLegend( 'Environment Settings' );
+		$form->addFieldset( $fieldset );
+		$this->setForm( $form );
+		//		$form->addFieldset( $fieldset );
+	//	$this->setForm( $form );
+    } 
+	// END OF CLASS
+}
