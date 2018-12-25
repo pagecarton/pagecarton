7be4b1c2a250838b6c325675edda82a85638e656
Ayoola FALOLA <ayoola@ayoo.la>
2018-12-25 23:42:57 +0100

    Fixed zoom level on classplayer

--- pagecarton/core/application/pages/includes/tools/classplayer.php
+++ pagecarton/core/application/pages/includes/tools/classplayer.php
@@ -9,20 +9,19 @@
 * @generated Ayoola_Page_Editor_Layout
 * @copyright  Copyright (c) PageCarton. (http://www.PageCarton.com)
 * @license    http://www.PageCarton.com/license.txt
-* @version $Id: classplayer.php	Thursday 8th of February 2018 12:33:46 AM	ayoola@ayoo.la $ 
+* @version $Id: classplayer.php	Tuesday 25th of December 2018 10:09:43 PM	ayoola@ayoo.la $ 
 */
 //	Page Include Content
 
 							if( Ayoola_Loader::loadClass( 'Ayoola_Object_Embed' ) )
 							{
 								
-$_d5da2e7732ed8481bf2f0f9454fc7a3f = new Ayoola_Object_Embed( array (
+$_f126b093afe0c23f9e3fa2eff412b539 = new Ayoola_Object_Embed( array (
   'editable' => 'Ayoola_Object_Play',
   'advanced_parameter_value' => 
   array (
     0 => 'pc_container',
   ),
-  'wrapper_name' => '',
   'object_class' => 'pc_container',
 ) );
 
@@ -30,7 +29,7 @@ $_d5da2e7732ed8481bf2f0f9454fc7a3f = new Ayoola_Object_Embed( array (
 							else
 							{
 								
-$_d5da2e7732ed8481bf2f0f9454fc7a3f = null;
+$_f126b093afe0c23f9e3fa2eff412b539 = null;
 
 							}
 							
