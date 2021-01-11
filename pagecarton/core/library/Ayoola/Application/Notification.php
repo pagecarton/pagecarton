fe9bfeaf9b6376d07fc9e03811fc60c271e7e11d
Ayoola FALOLA <ayoola@ayoo.la>
2021-01-11 14:05:27 +0100

    if email is set, use that email only

--- pagecarton/core/library/Ayoola/Application/Notification.php
+++ pagecarton/core/library/Ayoola/Application/Notification.php
@@ -66,33 +66,35 @@ class Ayoola_Application_Notification extends Ayoola_Abstract_Table
 	public static function getEmails()
     {
         //	also sent the message to all admin accounts
-        $emails = Application_Settings_CompanyInfo::getSettings( 'CompanyInformation', 'email' );
-        $userEmail = null;
+        if( ! $emails = Application_Settings_CompanyInfo::getSettings( 'CompanyInformation', 'email' ) )
+        {
+            $userEmail = null;
 
-        if( ! empty( Ayoola_Application::$GLOBAL['user']['email'] ) )
-        {
-            $userEmail = Ayoola_Application::$GLOBAL['user']['email'];
-        }
-        elseif( 
-                Ayoola_Application::getUserInfo( 'access_level' ) == 99  
-                || Ayoola_Application::getUserInfo( 'access_level' ) == 98
-        )
-        {
-            $userEmail = Ayoola_Application::getUserInfo( 'email' );
-        }
-        elseif( ! $userEmail = Ayoola_Application::getDomainSettings( 'email' ) )
-        {
-            $userTable = 'Ayoola_Access_LocalUser';
-            $userTable = $userTable::getInstance( $userTable::SCOPE_PROTECTED );
-            $userTable->getDatabase()->getAdapter()->setAccessibility( $userTable::SCOPE_PROTECTED );
-            $userTable->getDatabase()->getAdapter()->setRelationship( $userTable::SCOPE_PROTECTED );
-            $users = $userTable->select( null, array( 'access_level' => array( 99 ) ) );
-            foreach( $users as $each )
+            if( ! empty( Ayoola_Application::$GLOBAL['user']['email'] ) )
             {
-                $emails .= ( ',' . $each['email'] );
+                $userEmail = Ayoola_Application::$GLOBAL['user']['email'];
             }
+            elseif( 
+                    Ayoola_Application::getUserInfo( 'access_level' ) == 99  
+                    || Ayoola_Application::getUserInfo( 'access_level' ) == 98
+            )
+            {
+                $userEmail = Ayoola_Application::getUserInfo( 'email' );
+            }
+            elseif( ! $userEmail = Ayoola_Application::getDomainSettings( 'email' ) )
+            {
+                $userTable = 'Ayoola_Access_LocalUser';
+                $userTable = $userTable::getInstance( $userTable::SCOPE_PROTECTED );
+                $userTable->getDatabase()->getAdapter()->setAccessibility( $userTable::SCOPE_PROTECTED );
+                $userTable->getDatabase()->getAdapter()->setRelationship( $userTable::SCOPE_PROTECTED );
+                $users = $userTable->select( null, array( 'access_level' => array( 99 ) ) );
+                foreach( $users as $each )
+                {
+                    $emails .= ( ',' . $each['email'] );
+                }
+            }
+            $emails .= ( ',' . $userEmail );
         }
-        $emails .= ( ',' . $userEmail );
 
         $emails = trim( $emails, ', ' );
 		return $emails;
