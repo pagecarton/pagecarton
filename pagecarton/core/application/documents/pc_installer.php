0ebbf73ba9a7c51d3c7f561fc86e2f56b71b793b
Ayoola Falola <ayoola.falola@yahoo.com>
2022-11-17 09:29:56 +0100

    ...

--- pagecarton/core/application/documents/pc_installer.php
+++ pagecarton/core/application/documents/pc_installer.php
@@ -285,8 +285,24 @@ if (
             //    Open the downloaded ( and zipped ) framework and save it on the server.
             //    ini_set('max_execution_time', 200*60);
             $phar   = 'PharData';
-            $backup = new $phar($filename);
-            $backup->extractTo(APPLICATION_DIR, null, true);
+            try
+            {
+                $backup = new $phar($filename);
+            }
+            catch( Exception $e )
+            {
+                $badnews = 'There is an error with the installer file - ' . $e->getMessage();
+                unlink( $filename );
+            }
+            try
+            {
+                $backup->extractTo(APPLICATION_DIR, null, true);
+            }
+            catch( Exception $e )
+            {
+                $badnews = 'Unable to extract the installer file - ' . $e->getMessage();
+                unlink( $filename );
+            }
 
             //    return preserved data
             //    ini_set('max_execution_time', 200*60);
