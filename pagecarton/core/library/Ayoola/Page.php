124ca2c68556f358e55706949181250a0abcca2b
Ayoola Falola <ayoola.falola@yahoo.com>
2023-01-05 20:51:02 +0100

    ...

--- pagecarton/core/library/Ayoola/Page.php
+++ pagecarton/core/library/Ayoola/Page.php
@@ -163,7 +163,8 @@ class Ayoola_Page extends Ayoola_Page_Abstract
 
 			if( $info = $table->selectOne( null, array( 'url' => $url ), array( 'work-arround-1-333' => true ) ) )
 			{ 
-
+				var_export( $info );
+				
 				if( ! is_array( $info['page_options'] ) )
 				{
 					$info['page_options'] = array();
