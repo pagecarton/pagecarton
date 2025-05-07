39bd9c35d5f12bdddace8d04d9ca3fb5691017e8
Ayoola Falola <ayoola@ayoo.la>
2025-05-07 15:41:02 +0100

    ...

--- pagecarton/core/library/Ayoola/Abstract/Playable.php
+++ pagecarton/core/library/Ayoola/Abstract/Playable.php
@@ -369,14 +369,9 @@ abstract class Ayoola_Abstract_Playable extends Ayoola_Abstract_Viewable impleme
                         }
                         elseif( stripos( $template, $values['placeholder_prefix'] . $eachKey ) !== false )
                         {
-                            var_export( $key );
-                            var_export( $eachKey );
-                            var_export( $eachValue );
 
                             foreach( $eachValue as $vKey => $eachValueV )
                             {
-                                var_export( $vKey );
-                                var_export( $eachValueV );
 
                                 if( ! is_scalar( $eachValueV ) )
                                 {
@@ -407,8 +402,15 @@ abstract class Ayoola_Abstract_Playable extends Ayoola_Abstract_Viewable impleme
                         }    
                         elseif( stripos( $numberedPostTheme, $values['placeholder_prefix'] . $eachKey ) !== false )
                         {
+                            var_export( $key );
+                            var_export( $eachKey );
+                            var_export( $eachValue );
+
                             foreach( $eachValue as $vKey => $eachValueV )
                             {
+                                var_export( $vKey );
+                                var_export( $eachValueV );
+
                                 if( ! is_scalar( $eachValueV ) )
                                 {
                                     continue; 
