15d54b83768fb2a56a6b5857ea2d6c4bd65a7b13
Ayoola Falola <ayoola@ayoo.la>
2025-05-07 16:42:16 +0100

    ...

--- pagecarton/core/application/modules/Application/Subscription/Checkout.php
+++ pagecarton/core/application/modules/Application/Subscription/Checkout.php
@@ -304,7 +304,7 @@ class Application_Subscription_Checkout extends Application_Subscription_Abstrac
 
         if( ! empty( $checkoutOptionName ) && Ayoola_Loader::loadClass( $checkoutOptionName ) )
         {
-            return $checkoutOptionName;
+            return $checkoutOptionName; 
         }
 
 		$className = __CLASS__ . '_' . $data['checkoutoption_name'];
