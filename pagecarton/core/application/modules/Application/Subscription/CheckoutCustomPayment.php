7cf43d26a47d5d36a900c577a13d991d85753798
Ayoola Falola <ayoola@ayoo.la>
2025-05-07 17:20:05 +0100

    ...

--- pagecarton/core/application/modules/Application/Subscription/CheckoutCustomPayment.php
+++ pagecarton/core/application/modules/Application/Subscription/CheckoutCustomPayment.php
@@ -44,10 +44,7 @@ class Application_Subscription_CheckoutCustomPayment extends Application_Subscri
 
             $api = $cart['checkout_info']['checkoutoption_name'];
             
-            var_export( $api );
             $api = self::filterApi( $api );
-            var_export( $api );
-
         
             if( empty( $api ) || ! class_exists( $api ) )
             {
