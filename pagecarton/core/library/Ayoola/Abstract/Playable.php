3459083c31fd6678282813e2c02302712ef128ed
Ayoola Falola <ayoola@ayoo.la>
2025-05-07 15:37:29 +0100

    ...

--- pagecarton/core/library/Ayoola/Abstract/Playable.php
+++ pagecarton/core/library/Ayoola/Abstract/Playable.php
@@ -371,6 +371,9 @@ abstract class Ayoola_Abstract_Playable extends Ayoola_Abstract_Viewable impleme
                         {
                             foreach( $eachValue as $vKey => $eachValueV )
                             {
+                                var_export( $eachKey );
+                                var_export( $eachValue );
+                                var_export( $vKey );
                                 var_export( $eachValueV );
 
                                 if( ! is_scalar( $eachValueV ) )
