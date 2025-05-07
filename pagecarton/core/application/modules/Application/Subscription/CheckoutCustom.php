f135bc63e5f796ef19225ca33aaeec69f7cb09c7
Ayoola Falola <ayoola@ayoo.la>
2025-05-07 16:23:55 +0100

    ...

--- pagecarton/core/application/modules/Application/Subscription/CheckoutCustom.php
+++ pagecarton/core/application/modules/Application/Subscription/CheckoutCustom.php
@@ -82,7 +82,7 @@ class Application_Subscription_CheckoutCustom extends Application_Subscription_C
             {
                 $nextUrl = Ayoola_Page::getHomePageUrl() . '/widgets/' . __CLASS__ . 'Payment';
             }
-            header( $this->getParameter( 'next_url' ) );
+            header( $nextUrl );
 
 
         }
